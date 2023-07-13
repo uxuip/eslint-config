b0e2a0153f25dfd1397e5e98a97fb93645c9fb03
uxuip <uxuip@outlook.com>
2023-07-13 16:31:38 +0800

    chore: up deps

---
+++ README.md
@@ -0,0 +1,1 @@
+extends [antfu/eslint-config](https://github.com/antfu/eslint-config)
--- packages/vue/index.js
+++ packages/vue/index.js
@@ -18,5 +18,24 @@ module.exports = {
     'vue/component-name-in-template-casing': ['error', 'PascalCase', {
       registeredComponentsOnly: false,
     }],
+    'vue/attributes-order': ['error', {
+      order: [
+        'DEFINITION',
+        'LIST_RENDERING',
+        'CONDITIONALS',
+        'RENDER_MODIFIERS',
+        'GLOBAL',
+        'UNIQUE',
+        'SLOT',
+        'TWO_WAY_BINDING',
+        'OTHER_DIRECTIVES',
+        'ATTR_DYNAMIC',
+        'ATTR_STATIC',
+        'ATTR_SHORTHAND_BOOL',
+        'EVENTS',
+        'CONTENT',
+      ],
+      alphabetical: true,
+    }],
   },
 }
